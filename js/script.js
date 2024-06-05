1fc8cb2fcd02d3285232b6d2f434c58276c048af
Adisak-KS <Adisak.General@gmail.com>
2024-06-05 21:46:51 +0700

    add my information

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // / ================================= typing animation  ==================================
 
 let typed = new Typed(".typing", {
-    strings: ["New Graduates", "Web Developer", "PHP Developer", "Backend Developer", "Full-Stack Developer", "Software Engineering"],
+    strings: ["New Graduates", "Web Developer", "PHP Developer", "Backend Developer", "Full-Stack Developer", "Software Engineer"],
     typeSpeed: 100,   // ความเร็วในการพิมพ์ตัวอักษร (มิลลิวินาที)
     backSpeed: 50,    // ความเร็วในการลบตัวอักษร (มิลลิวินาที)
     backDelay: 500,
